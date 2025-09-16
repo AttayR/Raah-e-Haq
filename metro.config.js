331d501137df281a9d95b4a18c450c2b772992de
AttayR <attayrasoolofficial99@gmail.com>
2025-09-16 22:00:35 +0500

    build fix

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,16 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [],
+  resolver: {
+    blockList: [
+      // Ignore CMake build directories that cause file watcher errors
+      /.*\/\.cxx\/.*/,
+      /.*\/build\/.*/,
+      /.*\/CMakeFiles\/.*/,
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
